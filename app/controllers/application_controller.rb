04faf841bb9c1413f7ee5ebd22bff1dc7341f70d
wbdana <william.b.dana@gmail.com>
2017-06-29 15:19:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require_relative '../models/post.rb'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,55 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  # get '/' do
+  #   erb :index
+  # end!
+
+  # New Form
+  get '/posts/new' do
+    erb :new
   end
+
+  # Create
+  post '/posts' do
+    # binding.pry
+    @post = Post.create(params[:post])
+    erb :new_post_index
+  end
+
+  ## Read methods
+
+  # Index method
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    # binding.pry
+    @post = Post.all.find{|post| post.id == params[:id].to_i}
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    # binding.pry
+    @post = Post.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    # binding.pry
+    @post = Post.find_by(id: params[:id])
+    @post.update(params[:post])
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by(id: params[:id])
+    @post.delete
+    erb :post_deleted
+  end
+
+
+
 end
